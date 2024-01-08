74da801273640645a461f3b106a6041c643b00c8
王治春 <1508169607@qq.com>
2024-01-08 20:30:06 +0800

    feat:样式优化

--- src/components/WishCard/index.tsx
+++ src/components/WishCard/index.tsx
@@ -6,9 +6,12 @@ export default ({
 })=>{
  return (
   <div className='wish-card'>
+    <div className='relative-container'>
     <img src={wishCardImg} alt="" className='wish-card-bg'/>
     <div className='his-name'>亲爱的: {hisName}</div>
     <div className='your-name'> {yourName} 贺</div>
+    </div>
+   
   </div>
  )
 }
