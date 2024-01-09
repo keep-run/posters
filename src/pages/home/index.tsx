fa32273644591bdb8dd6278b123695a7be7018a4
王治春 <1508169607@qq.com>
2024-01-09 22:48:48 +0800

    feat:增加首页动画

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,15 +3,17 @@ import "@/common/common.less";
 // import { ControlBar, Player, PlayToggle } from "video-react";
 import "./index.less";
 // import VideoPlayer from '@/components/VideoPlayer';
-import gif from "@/assets/test.gif";
+import bg from "@/assets/firstPage/bg.png";
+import mov from "@/assets/firstPage/mov.png";
+import text1 from "@/assets/firstPage/text1.png";
+import text2 from "@/assets/firstPage/text2.png";
 import { useEffect } from "react";
 import { history } from "umi";
-
 export default () => {
   useEffect(() => {
     setTimeout(() => {
       history.replace("/guide");
-    }, 2000);
+    }, 6000);
   }, []);
   return (
     <div className="home-container">
@@ -23,7 +25,18 @@ export default () => {
           <PlayToggle />
         </ControlBar>
       </Player> */}
-      <img src={gif} alt="" />
+      <img
+        src={bg}
+        alt=""
+        style={{
+          width: "100%",
+        }}
+      />
+
+      <img src={mov} alt="" className="mov-img" />
+
+      <img src={text1} alt="" className="text1-img" />
+      <img src={text2} alt="" className="text2-img" />
       {/* <VideoPlayer videoSrc={vidioSrc} /> */}
     </div>
   );
