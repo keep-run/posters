2f0a51938985eba6d467377877a8c02d9a1b5eb0
王治春 <1508169607@qq.com>
2024-01-15 22:44:17 +0800

    feat:视频位置调整

--- .umirc.ts
+++ .umirc.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "umi";
 
 export default defineConfig({
   routes: [
-    { path: "/", component: "@/pages/home/index", layout: false },
-    { path: "/anim", component: "@/pages/animation/index", layout: false },
+    // { path: "/", component: "@/pages/home/index", layout: false },
+    { path: "/", component: "@/pages/animation/index", layout: false },
     { path: "/guide", component: "@/pages/guide/index", layout: false },
     { path: "/docs", component: "docs" },
     { path: "/test", component: "@/pages/test/index", layout: false },
