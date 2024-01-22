dc1f5318ae2afeabc975e7393353cbd0188e3712
王治春 <1508169607@qq.com>
2024-01-22 22:50:28 +0800

    feat:template2 图片丰富

--- src/const/imgInfo.ts
+++ src/const/imgInfo.ts
@@ -2,17 +2,17 @@ export const IMG_INFO = [
   {
     templateId: 1,
     templateBg: require("@/assets/set1/template.png"),
-    diyBg:require("@/assets/set1/diy-bg.png"),
+    diyBg: require("@/assets/set1/diy-bg.png"),
     wishBg: require("@/assets/wishBg.png"),
-    wishText:'愿得长如此\n年年物候新',
+    wishText: "愿得长如此\n年年物候新",
     icons: [
       {
         id: "1",
         url: require("@/assets/set1/icon1.png"),
         isFooter: true,
         style: {
-          width: '60%',
-        }
+          width: "60%",
+        },
       },
       {
         id: "2",
@@ -20,22 +20,22 @@ export const IMG_INFO = [
         isFooter: false,
         style: {
           position: "absolute",
-          top: '55%',
-          left:'7%',
-          width: '49%',
-        }
+          top: "55%",
+          left: "7%",
+          width: "49%",
+        },
       },
-     
+
       {
         id: "3",
         url: require("@/assets/set1/icon3.png"),
         isFooter: false,
         style: {
           position: "absolute",
-          top: '59%',
-          left:'40%',
-          width: '60%',
-        }
+          top: "59%",
+          left: "40%",
+          width: "60%",
+        },
       },
       // {
       //   id: "4",
@@ -53,65 +53,65 @@ export const IMG_INFO = [
           // position: "absolute",
           // top: '67%',
           // left:'21%',
-          width: '26%',
-        }
+          width: "26%",
+        },
       },
       {
         id: "5",
         url: require("@/assets/set1/icon5.png"),
         isFooter: true,
         style: {
-          width: '50%',
-        }
+          width: "50%",
+        },
       },
       {
         id: "6",
         url: require("@/assets/set1/icon6.png"),
         isFooter: true,
         style: {
-          width: '35%',
-        }
+          width: "35%",
+        },
       },
-     {
+      {
         id: "7",
         url: require("@/assets/set1/icon1-1.png"),
         isFooter: true,
         style: {
-          width: '17%',
-        }
+          width: "17%",
+        },
       },
       {
         id: "8",
         url: require("@/assets/set2/icon1.png"),
         isFooter: true,
         style: {
-          width: '66%',
-        }
+          width: "66%",
+        },
       },
       {
         id: "9",
         url: require("@/assets/set2/icon2.png"),
         isFooter: true,
         style: {
-          width: '44%',
-        }
+          width: "44%",
+        },
       },
       {
         id: "10",
         url: require("@/assets/set1/icon5-1.png"),
         isFooter: true,
         style: {
-          width: '20%',
-        }
+          width: "20%",
+        },
       },
     ],
   },
   {
     templateId: 2,
     templateBg: require("@/assets/set2/template.png"),
-    diyBg:require("@/assets/set2/diy-bg.png"),
+    diyBg: require("@/assets/set2/diy-bg.png"),
     wishBg: require("@/assets/set2/wish.png"),
-    wishText:'岁岁年年, 共欢同乐\n嘉庆与时新',
+    wishText: "岁岁年年, 共欢同乐\n嘉庆与时新",
     icons: [
       {
         id: "1",
@@ -119,10 +119,10 @@ export const IMG_INFO = [
         isFooter: false,
         style: {
           position: "absolute",
-          top: '61%',
-          left:'28%',
-          width: '66%',
-        }
+          top: "61%",
+          left: "28%",
+          width: "66%",
+        },
       },
       {
         id: "2",
@@ -130,10 +130,10 @@ export const IMG_INFO = [
         isFooter: false,
         style: {
           position: "absolute",
-          top: '57%',
-          left:'4%',
-          width: '44%',
-        }
+          top: "57%",
+          left: "4%",
+          width: "44%",
+        },
       },
       {
         id: "3",
@@ -141,16 +141,16 @@ export const IMG_INFO = [
         isFooter: true,
         style: {
           position: "absolute",
-          width: '50%',
-        }
+          width: "50%",
+        },
       },
       {
         id: "4",
         url: require("@/assets/set3/icon5-1.png"),
         isFooter: true,
         style: {
-          width: '20%',
-        }
+          width: "20%",
+        },
       },
       {
         id: "5",
@@ -158,82 +158,113 @@ export const IMG_INFO = [
         isFooter: false,
         style: {
           position: "absolute",
-          top: '67%',
-          left:'21%',
-          width: '26%',
-        }
+          top: "67%",
+          left: "21%",
+          width: "26%",
+        },
       },
       {
         id: "6",
         url: require("@/assets/set2/icon6.png"),
         isFooter: true,
         style: {
-          width: '60%',
-        }
+          width: "60%",
+        },
       },
       {
         id: "7",
         url: require("@/assets/set1/icon1-1.png"),
         isFooter: true,
         style: {
-          width: '20%',
-        }
-      },{
+          width: "20%",
+        },
+      },
+      {
         id: "8",
         url: require("@/assets/set2/icon8.png"),
         isFooter: true,
         style: {
-          width: '35%',
-        }
+          width: "35%",
+        },
       },
       {
         id: "9",
         url: require("@/assets/set1/icon2.png"),
         isFooter: true,
         style: {
-          width: '49%',
-        }
+          width: "49%",
+        },
       },
       {
         id: "10",
         url: require("@/assets/set1/icon3.png"),
         isFooter: true,
         style: {
-          width: '60%',
-        }
+          width: "60%",
+        },
       },
       {
         id: "11",
         url: require("@/assets/set1/icon5.png"),
         isFooter: true,
         style: {
-          width: '50%',
-        }
+          width: "50%",
+        },
+      },
+      {
+        id: "12",
+        url: require("@/assets/set2/icon9.png"),
+        isFooter: false,
+        style: {
+          left: "78%",
+          top: "59%",
+          position: "absolute",
+          width: "17%",
+        },
+      },
+      {
+        id: "13",
+        url: require("@/assets/set2/icon10.png"),
+        isFooter: true,
+        style: {
+          width: "20%",
+        },
+      },
+      {
+        id: "14",
+        url: require("@/assets/set2/title.png"),
+        isFooter: false,
+        style: {
+          width: "50%",
+          position: "absolute",
+          top: "12%",
+          left: "25%",
+        },
       },
     ],
   },
   {
     templateId: 3,
     templateBg: require("@/assets/set3/template.png"),
-    diyBg:require("@/assets/set3/diy-bg.png"),
+    diyBg: require("@/assets/set3/diy-bg.png"),
     wishBg: require("@/assets/wishBg.png"),
-    wishText:"日有熹，月有光\n富且昌，寿而康",
+    wishText: "日有熹，月有光\n富且昌，寿而康",
     icons: [
       {
         id: "1",
         url: require("@/assets/set3/icon1.png"),
         isFooter: true,
         style: {
-          width: '50%',
-        }
+          width: "50%",
+        },
       },
       {
         id: "2",
         url: require("@/assets/set1/icon5-1.png"),
         isFooter: true,
         style: {
-          width: '20%',
-        }
+          width: "20%",
+        },
       },
       {
         id: "3",
@@ -241,10 +272,10 @@ export const IMG_INFO = [
         isFooter: false,
         style: {
           position: "absolute",
-          top: '59%',
-          left:'10%',
-          width: '60%',
-        }
+          top: "59%",
+          left: "10%",
+          width: "60%",
+        },
       },
       {
         id: "4",
@@ -252,52 +283,51 @@ export const IMG_INFO = [
         isFooter: false,
         style: {
           position: "absolute",
-          top: '57%',
-          left:'59%',
-          width: '35%',
-        }
+          top: "57%",
+          left: "59%",
+          width: "35%",
+        },
       },
       {
         id: "5",
         url: require("@/assets/set3/icon5.png"),
         isFooter: true,
         style: {
-          width: '50%',
-        }
+          width: "50%",
+        },
       },
       {
         id: "6",
         url: require("@/assets/set2/icon3-1.png"),
         isFooter: true,
         style: {
-          width: '18%',
-        }
+          width: "18%",
+        },
       },
       {
         id: "7",
         url: require("@/assets/set3/icon7.png"),
         isFooter: true,
         style: {
-          width: '50%',
-        }
+          width: "50%",
+        },
       },
       {
         id: "8",
         url: require("@/assets/set1/icon1.png"),
         isFooter: true,
         style: {
-          width: '60%',
-        }
+          width: "60%",
+        },
       },
       {
         id: "9",
         url: require("@/assets/set1/icon7.png"),
         isFooter: true,
         style: {
-          width: '40%',
-        }
+          width: "40%",
+        },
       },
     ],
   },
 ];
-
